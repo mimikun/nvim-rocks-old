a5e887e52a11a077ceab070673eb31a98c8d1caa
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-05 14:33:23 +0900

    up

---
+++ lua/plugins/sweetie-nvim.lua
@@ -0,0 +1,15 @@
+vim.g.sweetie = {
+    pumblend = {
+        enable = true,
+        transparency_amount = 100,
+    },
+    integrations = {
+        lazy = true,
+        neorg = true,
+        neogit = true,
+        neomake = true,
+        telescope = true,
+    },
+    cursor_color = true,
+    terminal_colors = true,
+}
