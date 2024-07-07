4d7e2e4500643532616883bfc42dc95c6ed078ed
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-07 18:09:35 +0900

    wip

---
+++ lua/plugins/convert-nvim.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>cn", "<cmd>ConvertFindNext<CR>", { desc = "Find next convertible unit" })
+vim.keymap.set("n", "<leader>cc", "<cmd>ConvertFindCurrent<CR>", { desc = "Find convertible unit in current line" })
