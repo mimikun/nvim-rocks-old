6e9f3d61901f2328a8faf10760845cb1c19577a5
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-07 22:05:59 +0900

    wip

--- lua/plugins/denops-vim.lua
+++
@@ -1,9 +0,0 @@
--- Use denops-shared-server if there are no human rights
-vim.g.denops_server_addr = not is_human_rights and "127.0.0.1:32123" or ""
-
-require("denops-lazy").setup({
-    wait_load = true,
-    wait_server = true,
-    wait_server_delay = 10,
-    wait_server_retry_limit = 100,
-})
