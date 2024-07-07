540663d9027fa3fe41abbbff7e33651423f2978a
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-07 17:46:06 +0900

    wip

---
+++ lua/plugins/vfiler-vim.lua
@@ -0,0 +1,11 @@
+require("vfiler/config").setup({
+    options = {
+        auto_cd = true,
+        auto_resize = true,
+        keep = true,
+        layout = "left",
+        name = "explorer",
+        width = 30,
+        columns = "indent,devicons,name",
+    },
+})
