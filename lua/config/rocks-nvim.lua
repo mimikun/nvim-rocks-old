a29fe091062a9c8c08c7c625749bdd6f3627745d
Yuto Tanaka <karaage_nennen@icloud.com>
2024-07-20 13:30:15 +0900

    wip

--- lua/config/rocks-nvim.lua
+++ lua/config/rocks-nvim.lua
@@ -1,33 +1,3 @@
---[[
-do
-    -- Specifies where to install/use rocks.nvim
-    local install_location = vim.fs.joinpath(vim.fn.stdpath("data"), "rocks")
-
-    -- Set up configuration options related to rocks.nvim (recommended to leave as default)
-    local rocks_config = {
-        rocks_path = vim.fs.normalize(install_location),
-    }
-
-    vim.g.rocks_nvim = rocks_config
-
-    -- Configure the package path (so that plugin code can be found)
-    local luarocks_path = {
-        vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
-        vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
-    }
-    package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
-
-    -- Configure the C path (so that e.g. tree-sitter parsers can be found)
-    local luarocks_cpath = {
-        vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
-        vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
-    }
-    package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
-
-    -- Load all installed plugins, including rocks.nvim itself
-    vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
-end
-
 -- If rocks.nvim is not installed then install it!
 if not pcall(require, "rocks") then
     local rocks_location = vim.fs.joinpath(vim.fn.stdpath("cache"), "rocks.nvim")
@@ -50,24 +20,28 @@ if not pcall(require, "rocks") then
 
     vim.fn.delete(rocks_location, "rf")
 end
-]]
 
+-- Specifies where to install/use rocks.nvim
+local install_location = vim.fs.joinpath(vim.fn.stdpath("data"), "rocks")
+
+-- Set up configuration options related to rocks.nvim (recommended to leave as default)
 local rocks_config = {
-    rocks_path = vim.env.HOME .. "/.local/share/nvim-rocks/rocks",
+    rocks_path = vim.fs.normalize(install_location),
 }
 
 vim.g.rocks_nvim = rocks_config
 
+-- Configure the package path (so that plugin code can be found)
 local luarocks_path = {
     vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
     vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
 }
 package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
 
+-- Configure the C path (so that e.g. tree-sitter parsers can be found)
 local luarocks_cpath = {
     vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
     vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
-    -- Remove the dylib and dll paths if you do not need macos or windows support
     vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.dylib"),
     vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.dylib"),
     vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.dll"),
@@ -75,24 +49,5 @@ local luarocks_cpath = {
 }
 package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
 
+-- Load all installed plugins, including rocks.nvim itself
 vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
---[[
-local global = require("core.global")
-
----@type string
-local lazy_root = table.concat({ global.data_dir, "lazy" }, global.path_sep)
-
----@type string
-local lazy_path = table.concat({ lazy_root, "lazy.nvim" }, global.path_sep)
-
-if not vim.uv.fs_stat(lazy_path) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazy_path,
-    })
-end
-]]
